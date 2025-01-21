a59e1b7a1907c5a291892e7c52ce9bf505e71a54
riobima222 <patriobimasuci354@gmail.com>
2025-01-21 06:20:27 +0000

    main / modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
