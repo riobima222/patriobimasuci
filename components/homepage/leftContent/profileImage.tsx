0375f2657bad0aa8aa20e92eb947a6b5add49314
riobima222 <patriobimasuci354@gmail.com>
2025-02-20 14:51:12 +0000

    main / change profile picture

--- components/homepage/leftContent/profileImage.tsx
+++ components/homepage/leftContent/profileImage.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
-export default function ProfileImage () {
-    return (
-      <Image
-        src={"/images/profile.webp"}
-        alt="profile-image"
-        width={300}
-        height={300}
-        className="sm:w-[17em] sm:h-[17em] w-[12em] h-[12em]"
-        priority={true}
-      />
-    );
+export default function ProfileImage() {
+  return (
+    <Image
+      src={"/images/profile.jpeg"}
+      alt="profile-image"
+      width={300}
+      height={300}
+      className="sm:w-[17em] sm:h-[17em] w-[12em] h-[12em]"
+      priority={true}
+    />
+  );
 }
